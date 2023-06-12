49790246b6eba6f50991bae5b8ae9541c7f2a809
gafur-demirci <gafur.gs.68@gmail.com>
2023-06-12 13:27:02 +0300

    different solution added via computed

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -14,18 +14,29 @@ Vue.createApp({
             else this.resultText = '37';
         }
     },
+    // computed: {
+    //     result() {
+    //         if (this.counter < 37) {
+    //             return "Not there yet";
+    //         } else if (this.counter === 37) {
+    //             return this.counter;
+    //         } else {
+    //             return "Too much!";
+    //         }
+    //     }
+    // },
     methods: {
-    updateCounter(value) {
-        switch (value) {
-            case 5:
-                this.counter += 5
-                break;
-            case 1:
-                this.counter += 1
-                break;
-        }
+        updateCounter(value) {
+            switch (value) {
+                case 5:
+                    this.counter += 5
+                    break;
+                case 1:
+                    this.counter += 1
+                    break;
+            }
 
+        }
     }
-}
 
 }).mount('#assignment')
