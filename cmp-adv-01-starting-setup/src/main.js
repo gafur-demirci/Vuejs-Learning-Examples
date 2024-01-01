1949b306b048bc65e11b632e14ec9fde3732a652
gafur-demirci <gafur.gs.68@gmail.com>
2024-01-01 14:55:12 +0300

    Slot type configuration added

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 // Vue app'a component bağlama (Global olarak tanımlama.)
 
 app.component('base-badge', BaseBadge);
-
+app.component('base-card',BaseCard);
 // Vue app'i app id'li yapıya bağlama
 app.mount('#app');
