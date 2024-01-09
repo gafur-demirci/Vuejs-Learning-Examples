b02519a8bd79d03c12c6c8e719685f0943eed507
gafur-demirci <gafur.gs.68@gmail.com>
2024-01-09 19:54:59 +0300

    Dummy resources data added

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,22 +1,30 @@
 <template>
-    <ul v-for="resource in storedResources" :key="resource.id">
-        <h1>{{ resource.title }}</h1>
-        <h3>{{ resource.description }}</h3>
-        <a :href="resource.link" target="_blank">Open {{ resource.title }}</a>
-    </ul>
+  <ul v-for="resource in storedResources" :key="resource.id">
+    <h1>{{ resource.title }}</h1>
+    <h3>{{ resource.description }}</h3>
+    <a :href="resource.link" target="_blank">Open {{ resource.title }}</a>
+  </ul>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            storedResources: [
-                {
-                    id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js documentation.', link: 'https://vuejs.org'
-                }
-            ]
-        }
-    }
-}
-
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
