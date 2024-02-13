1acea7e40155fcbd93504997053b4f034bdbb47d
gafur-demirci <gafur.gs.68@gmail.com>
2024-02-13 16:55:19 +0300

    Vue router package added and related using pages imported

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const app = createApp(App)
 
