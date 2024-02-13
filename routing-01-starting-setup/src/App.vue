3cc1f7c345c8981941e3fca71dc6ef9c3d12afb2
gafur-demirci <gafur.gs.68@gmail.com>
2024-02-13 16:57:49 +0300

    Commented logic for route the pages in script because using vue router

--- routing-01-starting-setup/src/App.vue
+++ routing-01-starting-setup/src/App.vue
@@ -6,19 +6,19 @@
 </template>
 
 <script>
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
+// import TeamsList from './components/teams/TeamsList.vue';
+// import UsersList from './components/users/UsersList.vue';
 import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
     TheNavigation,
-    TeamsList,
-    UsersList,
+    // TeamsList,
+    // UsersList,
   },
   data() {
     return {
-      activePage: 'teams-list',
+      // activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -39,11 +39,11 @@ export default {
       users: this.users,
     };
   },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
-  },
+  // methods: {
+  //   setActivePage(page) {
+  //     this.activePage = page;
+  //   },
+  // },
 };
 </script>
 
