57c526ad0710dadc8b0badff8a009409a70bdfea
gafur-demirci <gafur.gs.68@gmail.com>
2024-02-22 21:24:06 +0300

    Some route config option on components config used components imported

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamFooter from './components/teams/TeamFooter.vue';
+import UserFooter from './components/users/UserFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
