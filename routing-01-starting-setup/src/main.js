856d88cb4af1f525d50a548af4f8b1236c0c57ba
gafur-demirci <gafur.gs.68@gmail.com>
2024-02-24 17:50:30 +0300

    afterEach added on router with use it when routes actions done

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -69,6 +69,12 @@ router.beforeEach((to, from, next) => {
   next();
 });
 
+router.afterEach((to, from) => {
+  // sending analytics data from to the server
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
 const app = createApp(App);
 
 app.use(router);
