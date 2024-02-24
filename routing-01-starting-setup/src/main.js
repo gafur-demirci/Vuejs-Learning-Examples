8be297bbc472ab3aba66d5586163772df038f4b4
gafur-demirci <gafur.gs.68@gmail.com>
2024-02-24 16:51:29 +0300

    Router scrollBehavior method added and configured for every route changes it when scroll to top or used back scroll to savedPositon (last position)

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -49,6 +49,16 @@ const router = createRouter({
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to);
+    console.log(from);
+    console.log(savedPosition);
+    if(savedPosition) return savedPosition;
+    return {
+      left: 0,
+      top: 0
+    }
+  },
 });
 
 const app = createApp(App);
